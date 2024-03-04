4205411327e8bc8027209e2560c51600884173f5
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2024-03-04 21:00:43 +0100

    restart

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,57 +1,32 @@
+import axios from 'axios';
 import { useEffect, useState } from 'react';
 
-import SearchBar from '../SearchBar/SearchBar';
-import toast, { Toaster } from 'react-hot-toast';
-import ImageGallery from '../ImageGallery/ImageGallery';
+// import SearchBar from '../SearchBar/SearchBar';
+// import toast, { Toaster } from 'react-hot-toast';
+// import ImageGallery from '../ImageGallery/ImageGallery';
 
-import { fetchGallery } from '../gallery-api';
+// import { fetchGallery } from '../gallery-api';
 
-const warning = () => {
-  toast.error('Please enter a searchquery', {
-    duration: 2000,
-    icon: '⚠️',
-  });
-};
+// const warning = () => {
+//   toast.error('Please enter a searchquery', {
+//     duration: 2000,
+//     icon: '⚠️',
+//   });
+// };
 
 function App() {
-  const [searchquery, setSearchQuery] = useState("");
-  const [page, setPage] = useState(1);
-  const [photos, setPhotos] = useState([]);
-
   useEffect(() => {
-    if (searchquery === '') {
-      warning();
-      return;
+    async function getPhotos() {
+      const response = await axios.get('https://api.unsplash.com/photos?client_id=WW6X1WrQmuJ-vE6R1rS70UQJ-uUsrM_fhzZ5r0cwV74');
+
+      const data = await response.data;
+      console.log(data);
     }
 
-    async function getData() {
-      try {
-        const data = await fetchGallery(searchquery,page);
-        setPhotos((prevPhotos) => {
-          return [...prevPhotos, ...data]
-        });
+    getPhotos();
+  }, []);
 
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-    getData();
-  }, [searchquery, page]);
-
-  const handleSearch = (newQuery) => {
-    setSearchQuery(newQuery);
-    setPage(1);
-    setPhotos([]);
-  };
-
-  return (
-    <>
-      <SearchBar onSearch={handleSearch} />
-      <Toaster />
-      <ImageGallery />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
